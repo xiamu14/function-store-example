b5580878eacf5ea4d655fda21a16f2e2f8a13302
ben <x_z_gfree@163.com>
2024-05-16 23:37:29 +0800

    feature: add packages.json for define project use function.

--- codegemrc.ts
+++ codegemrc.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "codegem";
 import { Ctx, FileType } from "codegem/build/type";
 import path from "path";
 import fse from "fs-extra";
+import { satisfies } from "compare-versions";
+
 export default defineConfig({
   // output: "src/fnStore", // 根目录;所有生成文件统一生成在这个目录下
   factory: [
     {
       name: "fnStore",
-      use: [loadFnStore()],
-      machine: createFn,
+      use: [loadFnStore(), loadFnMeta()],
+      machine: createFn as any,
     },
   ],
 });
@@ -24,23 +26,39 @@ function loadFnStore() {
   };
 }
 
-function createFn(
-  metaList: {
-    filename: string;
-    directory: string;
-    code: string;
-    version: string;
-    description: string;
-  }[][],
-): FileType[] {
+function loadFnMeta() {
+  return async (_: Ctx) => {
+    const _module = await import(path.join(process.cwd(), "src/fnStore/packages.json"), {
+      assert: { type: "json" },
+    });
+    console.log("loadFnMeta", _module.default);
+    return _module.default;
+  };
+}
+
+type MetaList = {
+  filename: string;
+  directory: string;
+  code: string;
+  version: string;
+  description: string;
+}[];
+function createFn([metaList, components]: [MetaList, Record<string, string>]): FileType[] {
   console.log(metaList);
   const root = path.join(process.cwd(), "src/fnStore");
   fse.ensureDirSync(root);
 
-  return metaList?.[0].map((item) => {
-    return {
-      pathname: path.join(root, item.directory, item.filename),
-      code: item.code,
-    };
-  });
+  return metaList
+    .filter((item) => {
+      const name = Object.keys(components).find((it) => new RegExp(it).test(item.filename)) as
+        | keyof typeof components
+        | undefined;
+      return name && satisfies(item.version, components[name]);
+    })
+    .map((item) => {
+      return {
+        pathname: path.join(root, item.directory, item.filename),
+        code: item.code,
+      };
+    });
 }
