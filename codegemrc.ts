09c23e69b394c2bb9c8f53204707ae99c5a03010
ben <x_z_gfree@163.com>
2024-05-24 13:58:46 +0800

    feature: use codgem-plugin-fnStore.

--- codegemrc.ts
+++ codegemrc.ts
@@ -1,64 +1,13 @@
 import { defineConfig } from "codegem";
-import { Ctx, FileType } from "codegem/build/type";
-import path from "path";
-import fse from "fs-extra";
-import { satisfies } from "compare-versions";
+import { loadFnStore, createFnStore } from "codegem-plugin-fnstore";
 
 export default defineConfig({
   // output: "src/fnStore", // 根目录;所有生成文件统一生成在这个目录下
   factory: [
     {
       name: "fnStore",
-      use: [loadFnStore(), loadFnPackages()],
-      machine: createFn as any,
+      use: [loadFnStore()],
+      machine: createFnStore,
     },
   ],
 });
-
-function loadFnStore() {
-  return async (_: Ctx) => {
-    const response = await fetch("https://function-store.zeabur.app/api/meta");
-    const result = await response.json();
-    if (result.errorCode === 0) {
-      return result.data;
-    }
-    return [];
-  };
-}
-
-function loadFnPackages() {
-  return async (_: Ctx) => {
-    const _module = await import(path.join(process.cwd(), "src/fnStore/packages.json"), {
-      assert: { type: "json" },
-    });
-    console.log("loadFnMeta", _module.default);
-    return _module.default;
-  };
-}
-
-type MetaList = {
-  filename: string;
-  directory: string;
-  code: string;
-  version: string;
-  description: string;
-}[];
-function createFn([metaList, components]: [MetaList, Record<string, string>]): FileType[] {
-  console.log(metaList);
-  const root = path.join(process.cwd(), "src/fnStore");
-  fse.ensureDirSync(root);
-
-  return metaList
-    .filter((item) => {
-      const name = Object.keys(components).find((it) => new RegExp(it).test(item.filename)) as
-        | keyof typeof components
-        | undefined;
-      return name && satisfies(item.version, components[name]);
-    })
-    .map((item) => {
-      return {
-        pathname: path.join(root, item.directory, item.filename),
-        code: item.code,
-      };
-    });
-}
